d4ac3fbdd86535a519042aab6982a3b5609c17f9
Jason Fricano <jasonfricano@Ninjas-MBP.local>
2020-10-16 23:59:05 +0800

    add energy()

--- SeamCarver.java
+++ SeamCarver.java
@@ -1,4 +1,5 @@
 import edu.princeton.cs.algs4.Picture;
+import edu.princeton.cs.algs4.StdOut;
 
 public class SeamCarver {
   private final Picture pic;
@@ -25,7 +26,30 @@ public class SeamCarver {
 
   // energy of pixel at column x and row y
   public double energy(int x, int y) {
-    return 0.0;
+    if (x == 0 || x == width()  - 1) return 1000;
+    if (y == 0 || y == height() - 1) return 1000;
+    
+    int[] left  = getRGB(x - 1, y);
+    int[] right = getRGB(x + 1, y);
+    int[] above = getRGB(x, y - 1);
+    int[] below = getRGB(x, y + 1);
+    int xGrad = 0;
+    int yGrad = 0;
+
+    for (int i = 0; i < left.length; i++) {
+      xGrad += Math.pow(left[i]  - right[i], 2);
+      yGrad += Math.pow(above[i] - below[i], 2);
+    }
+    return Math.sqrt(xGrad + yGrad);
+  }
+
+  // returns RGB values of passed (x,y) pixel as an array of doubles
+  private int[] getRGB(int x, int y) {
+    int rgb = pic.getRGB(x, y);
+    int r = (rgb >> 16) & 0xFF;
+    int g = (rgb >>  8) & 0xFF;
+    int b =  rgb        & 0xFF;
+    return new int[] { r, g, b };
   }
 
   // sequence of indices for horizontal seam
@@ -50,7 +74,13 @@ public class SeamCarver {
 
   // unit testing (optional)
   public static void main(String[] args) {
-
+    Picture p = new Picture("test/4x6.png");
+    SeamCarver sc = new SeamCarver(p);
+    for (int i = 0; i < p.width(); i++)
+      for (int j = 0; j < p.height(); j++) {
+        for (int k = 0; k < 3; k++)
+          StdOut.print(sc.getRGB(i, j)[k] + "\t");
+        StdOut.println();
+      }
   }
-
 }
